c7bbc2d6d1ff7b5dc6952af1b49171273dfa31a0
CarenTolentino <21-00883@g.batstate-u.edu.ph>
2023-11-21 23:37:59 +0800

    Updated Routing module and logo.page.ts
    Screen timeout is set to logo.page.ts for splashscreen

--- src/app/pages/logo/logo.page.ts
+++ src/app/pages/logo/logo.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-logo',
   templateUrl: './logo.page.html',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogoPage implements OnInit {
 
-  constructor() { }
+  constructor( private router: Router) { }
 
   ngOnInit() {
+    setTimeout( () => {
+      this.router.navigateByUrl('./login');
+
+    }, 3000);
   }
 
 }
