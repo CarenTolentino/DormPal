9e863f105c911d7bee08eb86e6ef1c031ec43617
OLLC137 <126338088+OLLC137@users.noreply.github.com>
2023-12-02 20:53:52 +0800

    many progress

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,5 +1,8 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -8,7 +11,18 @@ import { Router } from '@angular/router';
 })
 export class RegisterPage implements AfterViewInit {
 
-  constructor(private router: Router) { }
+  register:FormGroup
+
+  constructor(private router: Router, private httpClient: HttpClient, public formBuilder: FormBuilder,private alertController:AlertController) { 
+    this.register = this.formBuilder.group({
+      fname:[''],
+      lname:[''],
+      number:[''],
+      pword:[''],
+      pwordC:[''],
+      usertype:['']
+    })
+  }
 
   ngAfterViewInit() {
     const sign_in_btn = document.querySelector("#sign-in-btn");
@@ -26,7 +40,46 @@ export class RegisterPage implements AfterViewInit {
     }
   }
 
+
+
+  async presentAlert(alertHeader:string, alertMessage:string, alertButtons?:any){
+    let button = 'RETURN'
+    if (alertButtons != null) button = alertButtons
+    const alert = await this.alertController.create({
+      header : alertHeader,
+      message: alertMessage,
+      buttons: [button]
+    })
+
+    await alert.present()
+  }
+  
+
+  registerSubmit() {
+    var data = JSON.stringify(this.register.value)
+    
+    console.log(data)
+
+    const headers = new HttpHeaders();
+    headers.set('Content-Type', 'application/json; charset=UTF-8');
+    headers.set('Access-Control-Allow-Origin', '*');
+    headers.set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT');
+
+    this.httpClient.post('https://dormpal.000webhostapp.com/register.php', data, {headers: headers}).subscribe((response) => {
+      if(response == 'r01') this.presentAlert("ERROR","User already exists with this phone number.")
+      if(response == 'r02') {
+        this.presentAlert("Success","Successfully Signed Up!","Proceed to Log In")
+        this.router.navigate(['login'])
+      }
+      if(response == 'r03') this.presentAlert("ERROR","Passwords Do Not Match.")
+      if(response == 'r04') this.presentAlert("ERROR","Missing Input Parameters.")
+    })
+  }
+
   login() {
     this.router.navigate(['login']);
   }
+
+  
+
 }
